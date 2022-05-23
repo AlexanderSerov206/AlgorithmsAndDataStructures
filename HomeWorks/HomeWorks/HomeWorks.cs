019e6ca21525b45b5fb6990ff917818cadee7ce0
Alexander Serov <zuaz@Yandex.ru>
2022-05-23 21:10:17 +0400

    Решено задание 3.1
    Создан класс "Point". Он имеет поля-координаты X и Y.
    Для того, чтобы расчитывать расстояние между парой точек и для создания случайных точек, создан класс "Line" с методом CalculateLength, который позволяет расчитать "длину" конкретной линии.
    Для того, чтобы массово обрабатывать операции с линиями создан класс "Lines" с методами GenerateLines и CalculateLengths. Они позволяют сгенерировать заданное кол-во линий и расчитать длину всех линий в списке. Также, оба метода возвращают время, потребовавшееся на работу метода.
    Аналогично для структур.

--- HomeWorks/HomeWorks/HomeWorks.cs
+++ HomeWorks/HomeWorks/HomeWorks.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace HomeWorks
 {
@@ -96,6 +98,68 @@ namespace HomeWorks
             } while (node != null);
 
             Console.WriteLine();
+        }
+        public static void HomeWork_3_1()
+        {
+            Random random = new Random();
+
+            int iterations = 50;
+            Console.WriteLine($"Количество итераций для получения более точных средних значений: {iterations}. Расчёты запущены.");
+
+            double avgGen100000Class = 0;
+            double avgGen100000Struct = 0;
+            double avgCalc100000Class = 0;
+            double avgCalc100000Struct = 0;
+            double avgGen200000Class = 0;
+            double avgGen200000Struct = 0;
+            double avgCalc200000Class = 0;
+            double avgCalc200000Struct = 0;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                LinesClass linesClass100000 = new LinesClass(random);
+                LinesStuct linesStuct100000 = new LinesStuct(random);
+                LinesClass linesClass200000 = new LinesClass(random);
+                LinesStuct linesStuct200000 = new LinesStuct(random);
+
+                long timeToGen100000Class = linesClass100000.GenerateLines(100000);
+                long timeToGen100000struct = linesStuct100000.GenerateLines(100000);
+                long timeToCalcDist100000Class = linesClass100000.CalculateLengths();
+                long timeToCalcDist100000Struct = linesClass100000.CalculateLengths();
+
+                long timeToGen200000Class = linesClass200000.GenerateLines(200000);
+                long timeToGen200000struct = linesStuct200000.GenerateLines(200000);
+                long timeToCalcDist200000Class = linesClass200000.CalculateLengths();
+                long timeToCalcDist200000Struct = linesClass200000.CalculateLengths();
+
+                avgGen100000Class += timeToGen100000Class;
+                avgGen100000Struct += timeToGen100000struct;
+                avgCalc100000Class += timeToCalcDist100000Class;
+                avgCalc100000Struct += timeToCalcDist100000Struct;
+                avgGen200000Class += timeToGen200000Class;
+                avgGen200000Struct += timeToGen200000struct;
+                avgCalc200000Class += timeToCalcDist200000Class;
+                avgCalc200000Struct += timeToCalcDist200000Struct;
+            }
+
+            avgGen100000Class /= iterations;
+            avgGen100000Struct /= iterations;
+            avgCalc100000Class /= iterations;
+            avgCalc100000Struct /= iterations;
+            avgGen200000Class /= iterations;
+            avgGen200000Struct /= iterations;
+            avgCalc200000Class /= iterations;
+            avgCalc200000Struct /= iterations;
+
+            Console.WriteLine("\nВремя генерации точек:\n");
+            Console.WriteLine("{0, -16} ║ {1, -23} ║ {2, -27} ║ {3, -9}", "Количество точек", "Время заполнения(класс)", "Время заполнения(структура)", "Отношение");
+            Console.WriteLine("{0, -16} ║ {1, -23} ║ {2, -27} ║ {3, -9:P2}", 100000, $"{avgGen100000Class}мс", $"{avgGen100000Struct}мс", avgGen100000Class / avgGen100000Struct);
+            Console.WriteLine("{0, -16} ║ {1, -23} ║ {2, -27} ║ {3, -9:P2}", 200000, $"{avgGen200000Class}мс", $"{avgGen200000Struct}мс", avgGen200000Class / avgGen200000Struct);
+
+            Console.WriteLine("\nВремя подсчёта дистанций:\n");
+            Console.WriteLine("{0, -16} ║ {1, -23} ║ {2, -27} ║ {3, -9}", "Количество точек", "Время заполнения(класс)", "Время заполнения(структура)", "Отношение");
+            Console.WriteLine("{0, -16} ║ {1, -23} ║ {2, -27} ║ {3, -9:P2}", 100000, $"{avgCalc100000Class}мс", $"{avgCalc100000Struct}мс", avgCalc100000Class / avgCalc100000Struct);
+            Console.WriteLine("{0, -16} ║ {1, -23} ║ {2, -27} ║ {3, -9:P2}", 200000, $"{avgCalc200000Class}мс", $"{avgCalc200000Struct}мс", avgCalc200000Class / avgCalc200000Struct);
         }
     }
 }
---
+++ HomeWorks/HomeWorks/LineClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks
+{
+    internal class LineClass
+    {
+        public PointClassDouble PointOne { get; private set; }
+        public PointClassDouble PointTwo { get; private set; }
+
+        /// <summary>
+        /// Конструктор, позволяющий генерировать отрезок из ранее созданных точек.
+        /// </summary>
+        /// <param name="pointOne"></param>
+        /// <param name="pointTwo"></param>
+        public LineClass(PointClassDouble pointOne, PointClassDouble pointTwo)
+        {
+            PointOne = pointOne;
+            PointTwo = pointTwo;
+        }
+
+        /// <summary>
+        /// Конструктор, генерирующий отрезок со случайными координатами точек.
+        /// </summary>
+        /// <param name="random"></param>
+        public LineClass(Random random)
+        {
+            PointOne = new PointClassDouble() { X = random.NextDouble(), Y = random.NextDouble() };
+            PointTwo = new PointClassDouble() { X = random.NextDouble(), Y = random.NextDouble() };
+        }
+
+        /// <summary>
+        /// Рассчитать длину отрезка.
+        /// </summary>
+        /// <returns></returns>
+        public double CalculateLength()
+        {
+            double x = PointOne.X - PointTwo.X;
+            double y = PointOne.Y - PointTwo.Y;
+
+            return Math.Sqrt((x * x) + (y * y));
+        }
+    }
+}
