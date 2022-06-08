028d98251b3f0fed4d448149fd8debfa2a556a2b
Alexander Serov <zuaz@Yandex.ru>
2022-06-08 16:29:53 +0400

    Решено задание 7
    Создан класс "Ячейка", у которой есть состояние. Она либо свободна, либо занята ферзём, либо заблокирована ферзём.
    Создан класс "Доска", который состоит из ячеек. У доски есть основной метод, который совершает попытку установить ферзя на указанную ячейку.
    Реализован консольный интерфейс, который позволяет произвольно задавать размерность доски, а так же печатать результаты в консоль.

---
+++ HomeWorks/HomeworksLibrary/HomeWork7/Cell.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworksLibrary.HomeWork7
+{
+    /// <summary>
+    /// Клетка шахматной доски
+    /// </summary>
+    internal class Cell
+    {
+        /// <summary>
+        /// Состояние клетки
+        /// </summary>
+        public State State { get; private set; }
+        public Cell()
+        {
+            State = State.Empty;
+        }
+
+        /// <summary>
+        /// Помечает ячейку как занятую ферзём.
+        /// </summary>
+        /// <returns>true, если успешно. false, если не успешно.</returns>
+        public bool SetOccupied()
+        {
+            if (State == State.Empty)
+            {
+                State = State.Occupied;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Помечает ячейку как заблокированную каким-либо ферзём.
+        /// </summary>
+        /// <returns>true, если успешно. false, если не успешно./returns>
+        public bool Lock()
+        {
+            if (State == State.Empty || State == State.Locked)
+            {
+                State = State.Locked;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
