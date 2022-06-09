7e59a86542f60a26f3a80a8d9e336b05ea9969cb
Alexander Serov <zuaz@Yandex.ru>
2022-06-09 19:39:44 +0400

    Решено задание 8
    Реализован ввод последовательности чисел в консоль, конвертация её в целочисленные значения и сортировка подсчётом.

---
+++ HomeWorks/HomeworksLibrary/HomeWork8/CountingSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworksLibrary.HomeWork8
+{
+    static class CountingSort
+    {
+        public static int[] Sort(int[] inArray)
+        {
+            int min = inArray.Min();
+            int max = inArray.Max();
+            int count = max - min + 1;
+
+            int[] countNumbers = new int[count];
+            int[] outArray = new int[inArray.Length];
+
+            for (int i = 0; i < inArray.Length; i++)
+            {
+                countNumbers[inArray[i] - min]++;
+            }
+
+            int iterator = 0;
+
+            for (int i = min; i <= max; i++)
+            {
+                while (countNumbers[i - min]-- > 0)
+                {
+                    outArray[iterator] = i;
+                    iterator++;
+                }
+            }
+
+            return outArray;
+        }
+    }
+}
---
+++ HomeWorks/HomeworksLibrary/HomeWork8/HomeWork8.cs
@@ -0,0 +1,61 @@
+﻿using IHomeworkLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworksLibrary.HomeWork8
+{
+    internal class HomeWork8 : IHomework
+    {
+        [Display(Name = "Задание 8", Description = "Сортировка подсчётом")]
+        public static void HomeWork_8()
+        {
+            Console.WriteLine("Введите целые числа, разделённые пробелом для сортировки.");
+
+            int[] sorted = CountingSort.Sort(GetArrayOfNumbers());
+
+            Console.WriteLine("Отсортированный массив:");
+            PrintIntArray(sorted);
+        }
+
+        private static int[] GetArrayOfNumbers()
+        {
+            string answer = Console.ReadLine();
+
+            return ConvertStringToIntsArray(answer);
+        }
+
+        private static int[] ConvertStringToIntsArray(string answer)
+        {
+            string[] separateStrings = answer.Split(' ');
+            List<int> numbers = new List<int>();
+
+            foreach (string item in separateStrings)
+            {
+                if (int.TryParse(item, out int itemAsNumber))
+                {
+                    numbers.Add(itemAsNumber);
+                }
+            }
+
+            return numbers.ToArray();
+        }
+
+        private static void PrintIntArray(int[] sorted)
+        {
+            StringBuilder sortedArray = new StringBuilder();
+
+            foreach (int item in sorted)
+            {
+                sortedArray.Append(item);
+                sortedArray.Append(", ");
+            }
+            sortedArray.Remove(sortedArray.Length - 2, 2);
+
+            Console.WriteLine(sortedArray);
+        }
+    }
+}
